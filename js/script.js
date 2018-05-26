5f0bd8a739dad975098771458b600f98b76e91da
Amaury <azapien@hotmail.com>
2018-05-26 07:54:06 -0500

    jquery algorithms

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    //set options
+    var speed = 500; //fade speed
+    var autoswitch = true; // auto slider options
+    var autoSwitch_speed = 4000 //auto  slider speed
+
+    //add initial active class
+    $('.slide').first().addClass('active');
+    
+    //hide all slides
+    $('.slide').hide();
+
+    //show first slide
+    $('.active').show();
+
+    //next handler
+    $('#next').on('click', nextSlide);
+
+    //prev handler
+    $('#prev').on('click', prevSlide);
+
+    //auto slider handler
+    if(autoswitch == true){
+        setInterval(nextSlide, autoSwitch_speed);
+    }
+
+    //Switch to next slide
+    function nextSlide(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        }
+        else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+
+    // switch to prev slide
+    function prevSlide(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':first-child')){
+            $('.slide').last().addClass('active');
+        }
+        else {
+            $('.oldActive').prev().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+});
